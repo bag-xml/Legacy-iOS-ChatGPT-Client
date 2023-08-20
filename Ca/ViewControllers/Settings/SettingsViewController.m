0c94affd2366352af5ce37ac8a246df9c185aa8a
Daphne <gaming.mali357@gmail.com>
2023-08-20 06:13:17 +0200

    Custom prompts work now.

--- Ca/ViewControllers/Settings/SettingsViewController.m
+++ Ca/ViewControllers/Settings/SettingsViewController.m
@@ -10,8 +10,7 @@
 
 @interface SettingsViewController () <UITextFieldDelegate>
 
-@property (strong, nonatomic) IBOutlet UITextField *inputText;
-@property (strong, nonatomic) IBOutlet UITextField *degenprompt;
+@property (weak, nonatomic) IBOutlet UITextField *gptPromptInput;
 
 @end
 //this is prime example of me failing :pleading:
@@ -20,46 +19,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    NSString *savedText = [[NSUserDefaults standardUserDefaults] objectForKey:@"apiKey"];
-    if (savedText) {
-        self.inputText.text = savedText;
+    NSString *savedPrompt = [[NSUserDefaults standardUserDefaults] objectForKey:@"gptPrompt"];
+    if (savedPrompt) {
+        self.gptPromptInput.text = savedPrompt;
     }
-    
-    self.inputText.delegate = self;
 }
 
 
-- (IBAction)submit:(id)sender {
-    //ok button code here, glad that it worked, it was just the splitview of xcode being fucked
-}
-
-
-
-
-
-#pragma mark - UITextFieldDelegate
-
-//this is the API Key box.
-//chatgpt my beloved
-- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:
-    //nsstring
-    (NSString *)string {
-    NSString *currentText = textField.text;
-    NSString *newText = [currentText stringByReplacingCharactersInRange:range withString:string];
-    
-    // save new text to pref
-    if (newText.length > 0) {
-        [[NSUserDefaults standardUserDefaults] setObject:newText forKey:@"apiKey"];
-    }
-//udpdaze api eyk
-    NSString *configPath = [[NSBundle mainBundle] pathForResource:@"info" ofType:@"plist"];
-    
-    NSMutableDictionary *configDict = [NSMutableDictionary dictionaryWithContentsOfFile:configPath];
-    configDict[@"apiKey"] = newText;
-    
-    [configDict writeToFile:configPath atomically:YES];
-
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    //yay
     return YES;
+}
+
+- (IBAction)submitPrompt:(id)sender {
+    NSString *prompt = self.gptPromptInput.text;
+    if (prompt.length > 0) {
+        [[NSUserDefaults standardUserDefaults] setObject:prompt forKey:@"gptPrompt"];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+    }
 }
 
 @end
