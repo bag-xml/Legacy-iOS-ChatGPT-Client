c9e1c4335ed150ff4aa87278303c939d9256360f
Daphne <gaming.mali357@gmail.com>
2023-08-22 02:40:44 +0200

    give up on app statistics

--- Ca/ViewControllers/Delegates/AppDelegate.m
+++ Ca/ViewControllers/Delegates/AppDelegate.m
@@ -45,7 +45,7 @@
         [defaults setObject:aiPrompt forKey:@"gptPrompt"];
     }
     if (![defaults objectForKey:@"requestAmount"]) {
-        [defaults setObject:requestAmount forKey:@"totalRequests"];
+        [defaults setObject:requestAmount forKey:@"requestAmount"];
     }
     [defaults synchronize];
     return YES;
