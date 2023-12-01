2818a1c7df63fcb0deb0ec632bb17bf2dc670611
bag-xml <gaming.mali357@gmail.com>
2023-12-01 21:39:39 +0100

    add basic layout to ipad

---
+++ Ca/Classes/iPad/Chat/CAiPadIsolatedChatView.h
@@ -0,0 +1,13 @@
+//
+//  CAiPadIsolatedChatView.h
+//  ChatGPT
+//
+//  Created by bag.xml on 01/12/23.
+//  Copyright (c) 2023 China DOS Union. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CAiPadIsolatedChatView : UIViewController
+
+@end
---
+++ Ca/Classes/iPad/Sidebar/CASidebarTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CASidebarTableViewController.h
+//  ChatGPT
+//
+//  Created by bag.xml on 01/12/23.
+//  Copyright (c) 2023 electron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CASidebarTableViewController : UITableViewController
+
+@end
